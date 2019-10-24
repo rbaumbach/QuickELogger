f19330840747f388a6a03d28e0424aad17b0b7d1
Ryan <github@ryan.codes>
2019-10-24 16:58:47 -0600

    Redo project

---
+++ QuickELogger/QuickELogger.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuickELogger.
+FOUNDATION_EXPORT double QuickELoggerVersionNumber;
+
+//! Project version string for QuickELogger.
+FOUNDATION_EXPORT const unsigned char QuickELoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickELogger/PublicHeader.h>
