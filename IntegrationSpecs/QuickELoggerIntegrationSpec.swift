1472f6e834df1ea80265322a86a38972e59bd324
Ryan <github@ryan.codes>
2019-10-26 16:54:36 -0600

    Create integration specs to test the QuickELogger interface

--- IntegrationSpecs/QuickELoggerIntegrationSpec.swift
+++ IntegrationSpecs/QuickELoggerIntegrationSpec.swift
@@ -7,13 +7,143 @@ class QuickELoggerIntegrationSpec: QuickSpec {
     override func spec() {
         describe("QuickELogger") {
             var subject: QuickELogger!
-                        
-            beforeEach {
-                subject = QuickELogger()
+            
+            beforeSuite {
+                emptyDocumentsDirectory()
             }
             
-            it("exists") {
-                expect(subject).toNot(beNil())
+            afterSuite {
+                emptyDocumentsDirectory()
+            }
+            
+            describe("when using the default filename for the log file") {
+                beforeEach {
+                    subject = QuickELogger()
+                }
+                
+                describe("when logging a message") {
+                    describe("when a log file doesn't exist on disk") {
+                        beforeEach {
+                            subject.log(message: "Goofus", type: .error)
+                        }
+                        
+                        afterEach {
+                            emptyDocumentsDirectory()
+                        }
+                        
+                        it("is logged in a new JSON file to disk") {
+                            let logMessages = getLogMessages()
+                                                        
+                            expect(logMessages.count).to(equal(1))
+                            
+                            let logMessage = logMessages.first!
+                            
+                            expect(logMessage.id).toNot(beNil())
+                            expect(logMessage.timeStamp).toNot(beNil())
+                            
+                            expect(logMessage.type).to(equal(.error))
+                            expect(logMessage.message).to(equal("Goofus"))
+                        }
+                    }
+                    
+                    describe("when a log file already exists on disk") {
+                        beforeEach {
+                            subject.log(message: "Goofus", type: .error)
+                            subject.log(message: "Gallant", type: .info)
+                        }
+                        
+                        afterEach {
+                            emptyDocumentsDirectory()
+                        }
+                        
+                        it("is logged with the previous JSON data to disk") {
+                            let logMessages = getLogMessages()
+                                                        
+                            expect(logMessages.count).to(equal(2))
+                            
+                            let previousLogMessage = logMessages[0]
+                            
+                            expect(previousLogMessage.id).toNot(beNil())
+                            expect(previousLogMessage.timeStamp).toNot(beNil())
+                            
+                            expect(previousLogMessage.type).to(equal(.error))
+                            expect(previousLogMessage.message).to(equal("Goofus"))
+                            
+                            let newLogMessage = logMessages[1]
+                            
+                            expect(newLogMessage.id).toNot(beNil())
+                            expect(newLogMessage.timeStamp).toNot(beNil())
+                            
+                            expect(newLogMessage.type).to(equal(.info))
+                            expect(newLogMessage.message).to(equal("Gallant"))
+                        }
+                    }
+                }
+            }
+            
+            describe("when using a user specified filename for the log file") {
+                beforeEach {
+                    subject = QuickELogger(filename: "mas-tacos")
+                }
+                
+                describe("when logging a message") {
+                    describe("when a log file doesn't exist on disk") {
+                        beforeEach {
+                            subject.log(message: "Goofus", type: .error)
+                        }
+                        
+                        afterEach {
+                            emptyDocumentsDirectory()
+                        }
+                        
+                        it("is logged in a new JSON file to disk") {
+                            let logMessages = getLogMessages(filename: "mas-tacos")
+                                                        
+                            expect(logMessages.count).to(equal(1))
+                            
+                            let logMessage = logMessages.first!
+                            
+                            expect(logMessage.id).toNot(beNil())
+                            expect(logMessage.timeStamp).toNot(beNil())
+                            
+                            expect(logMessage.type).to(equal(.error))
+                            expect(logMessage.message).to(equal("Goofus"))
+                        }
+                    }
+                    
+                    describe("when a log file already exists on disk") {
+                        beforeEach {
+                            subject.log(message: "Goofus", type: .error)
+                            subject.log(message: "Gallant", type: .info)
+                        }
+                        
+                        afterEach {
+                            emptyDocumentsDirectory()
+                        }
+                        
+                        it("is logged with the previous JSON data to disk") {
+                            let logMessages = getLogMessages(filename: "mas-tacos")
+                                                        
+                            expect(logMessages.count).to(equal(2))
+                            
+                            let previousLogMessage = logMessages[0]
+                            
+                            expect(previousLogMessage.id).toNot(beNil())
+                            expect(previousLogMessage.timeStamp).toNot(beNil())
+                            
+                            expect(previousLogMessage.type).to(equal(.error))
+                            expect(previousLogMessage.message).to(equal("Goofus"))
+                            
+                            let newLogMessage = logMessages[1]
+                            
+                            expect(newLogMessage.id).toNot(beNil())
+                            expect(newLogMessage.timeStamp).toNot(beNil())
+                            
+                            expect(newLogMessage.type).to(equal(.info))
+                            expect(newLogMessage.message).to(equal("Gallant"))
+                        }
+                    }
+                }
             }
         }
     }
---
+++ IntegrationSpecs/Utils/Utils.swift
@@ -0,0 +1,50 @@
+import Foundation
+@testable import QuickELogger
+
+func documentsDirectory() -> URL {
+    return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+}
+
+func fullJSONFilepath(filename: String) -> URL {
+    return documentsDirectory().appendingPathComponent("\(filename).json")
+}
+
+func emptyDocumentsDirectory() {
+    let documentsDirectoryPath = documentsDirectory().path
+    
+    let fileManager = FileManager.default
+    
+    let allFilePathsInDocumentsDirectory = try! fileManager.contentsOfDirectory(atPath: documentsDirectoryPath)
+    
+    allFilePathsInDocumentsDirectory.forEach { documentPath in
+        try! fileManager.removeItem(atPath: documentsDirectoryPath + "/" + documentPath)
+    }
+}
+
+func getLogMessages(filename: String = "Quick-E-Logger") -> [LogMessage] {
+    let fullPathOfJSONFile = fullJSONFilepath(filename: filename)
+    
+    guard let jsonData = try? Data(contentsOf: fullPathOfJSONFile, options: .alwaysMapped) else {
+        return []
+    }
+    
+    let jsonDecoder = JSONDecoder()
+    jsonDecoder.dateDecodingStrategy = .iso8601
+
+    guard let logMessages = try? jsonDecoder.decode([LogMessage].self, from: jsonData) else {
+        return []
+    }
+
+    return logMessages
+}
+
+func saveLogMessages(filename: String, messages: [LogMessage]) {
+    let fullPathOfJSONFile = fullJSONFilepath(filename: filename)
+    
+    let jsonEncoder = JSONEncoder()
+    jsonEncoder.dateEncodingStrategy = .iso8601
+    
+    let encodedJSONData = try! jsonEncoder.encode(messages)
+    
+    try! encodedJSONData.write(to: fullPathOfJSONFile, options: .atomic)
+}
